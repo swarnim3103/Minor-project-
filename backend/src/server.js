9c04a69b2dda6c4f9cc783908935bc7da61e4d6c
swarnim3103 <143250446+swarnim3103@users.noreply.github.com>
2026-09-14 23:57:13 +0530

    test api setup route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const testCallRoutes = require('./routes/testCall.routes');
 const authRoutes = require('./routes/auth.routes');
 const dashboardRoutes = require('./routes/dashboard.routes');
 const pool = require('./config/db');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use('/api', testCallRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
